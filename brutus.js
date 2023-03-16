7d876d05de35ae4038ece70a21e0f9a7bf3d59d7
Jas <55251065+jaswpratt@users.noreply.github.com>
2023-03-15 21:13:57 -0400

    Create brutus.js

---
+++ brutus.js
@@ -0,0 +1,160 @@
+/**
+ * nugatoria.js
+ * @author Sabine
+ * https://stackoverflow.com/questions/4308554/simplest-way-to-read-json-from-a-url-in-java
+ */
+   var count = {id : "10", value: "10"};
+   var category = {id : "any", value: "Any Category"};
+   var type = {id : "any", value: "Any Type"};
+   var difficulty = {id : "any", value: "Any Difficulty"};
+   var results = "";
+   var triviaQuestionList = new Array();  // Contains js objects of Questions and Answers
+   var panelQuestions = new Array();      // Contains html to display each Question and Answers
+   var submittedAnswers = new Array();    // Contains the Answers submitted to each question
+   var currentQuestionIndex = 0;
+   var triviaQuestion = {
+         category : "",
+         question : "",
+         type : "",
+         correctAnswer : "",
+         incorrectAnswers : new Array(),
+         selectedAnswer : "",
+         difficulty : ""
+   }
+
+   $( document ).ready(function() {
+      $('#gameQuestions').hide();
+      $('#count').combobox({
+         url:'json/count.json',
+         valueField:'id',
+         textField:'count',
+         onSelect: function(rec){
+            count.id = rec.id;
+            count.value = rec.count;
+         }
+      });
+      $('#category').combobox({
+         url:'json/category.json',
+         valueField:'id',
+         textField:'category',
+         onSelect: function(rec){
+            category.id = rec.id;
+            category.value = rec.category;
+         }
+      });
+      $('#type').combobox({
+          url:'json/type.json',
+          valueField:'id',
+          textField:'type',
+          onSelect: function(rec){
+             type.id = rec.id;
+             type.value = rec.type;
+          }
+       });
+      $('#difficulty').combobox({
+          url:'json/difficulty.json',
+          valueField:'id',
+          textField:'difficulty',
+          onSelect: function(rec){
+             difficulty.id = rec.id;
+             difficulty.value = rec.difficulty;
+          }
+     });
+     $('#getTriviaQuiz').linkbutton({
+        iconCls: 'searchbox',
+        onClick: function(){
+           $.post("trivia",{
+                  count: count.id,
+                  category: category.id,
+                  type: type.id,
+                  difficulty: difficulty.id
+           }, function(data, status, xhr){
+              triviaQuizCallback(data, status);
+           }, "json");
+        } 
+     });
+   });
+
+   function triviaQuizCallback(data, status){
+      if(data.length == 0){
+         var message = "Unable to get trivia questions for:";
+         message += "\n  - count = " + count.id;
+         message += "\n  - category = " + category.id;
+         message += "\n  - type = " + type.id;
+         message += "\n  - difficulty = " + difficulty.id;
+         alert(message);
+         return null;
+      }
+
+      data.forEach(createQuestionList);
+      createQuestionPanel(triviaQuestionList);
+      playTriviaGame();
+   }
+   
+   function createQuestionList(question) {
+      triviaQuestion = {  //Create a new triviaQuestion object each time
+         category : "",
+         question : "",
+         type : "",
+         correctAnswer : "",
+         incorrectAnswers : new Array(),
+         selectedAnswer : "",
+         difficulty : ""
+      }
+      triviaQuestion.category = question.category;
+      triviaQuestion.question = question.question;
+      triviaQuestion.type = question.type;
+      triviaQuestion.correctAnswer = question.correctAnswer;
+      triviaQuestion.incorrectAnswers = question.incorrectAnswers;
+      triviaQuestion.difficulty = question.difficulty;
+      triviaQuestionList.push(triviaQuestion);     
+   }
+
+   function createQuestionPanel(questionList){
+      panelQuestions = new Array();
+      var questionHtml = "";
+      for(var i= 0; i < questionList.length; i++){
+         questionHtml = "";
+         questionHtml += "<span id='tq'" + i + ">" + questionList[i].question + "</span>\n<p>";
+         questionHtml += "<ol type='a'>\n";
+         questionHtml += "   <li><input type='radio' id='a' name='answer' value='"+ questionList[i].incorrectAnswers[0] + "'>  " + questionList[i].incorrectAnswers[0] + "</input></li>\n";
+         questionHtml += "   <li><input type='radio' id='b' name='answer' value='"+ questionList[i].correctAnswer + "'>  " + questionList[i].correctAnswer + "</input></li>\n";
+         questionHtml += "   <li><input type='radio' id='c' name='answer' value='"+ questionList[i].incorrectAnswers[1] + "'>  " + questionList[i].incorrectAnswers[1] + "</input></li>\n";
+         questionHtml += "   <li><input type='radio' id='d' name='answer' value='"+ questionList[i].incorrectAnswers[2] + "'>  " + questionList[i].incorrectAnswers[2] + "</input></li>\n";
+         questionHtml += "</ol>";
+         panelQuestions.push(questionHtml);
+      }
+   }
+   
+   function playTriviaGame(){
+      currentQuestionIndex = 0;
+      submittedAnswers = new Array(triviaQuestionList.length);
+      $('#gameQuestionPanel').html(panelQuestions[currentQuestionIndex]);
+      $('#gameQuestions').show();
+   }
+   
+   $(function(){
+       var i = 0;
+       $('#btnNextQuestion').bind('click', function(){
+          if (i < panelQuestions.length){
+        	 currentQuestionIndex = i++;
+             $('#gameQuestionPanel').html(panelQuestions[currentQuestionIndex]);
+          }
+       });
+   });
+   
+   $(function(){
+       var i = 0;
+       $('#btnLastQuestion').bind('click', function(){
+          if (i < panelQuestions.length){
+        	 currentQuestionIndex = i--;
+             $('#gameQuestionPanel').html(panelQuestions[currentQuestionIndex]);
+          }
+       });
+   });
+   
+   $(function(){
+       $('#btnSubmitQuestion').bind('click', function(){
+    	   submittedAnswers[currentQuestionIndex] = $("input[name='answer']:checked").val();
+       });
+   });
